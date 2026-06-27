43482e05175df6e396f07dd49c69c7005a6b4c86
Arshad A K <arshadmdvr@gmail.com>
2026-06-27 21:19:54 +0530

    Added release folder and contents

---
+++ release/raycast-env.d.ts
@@ -0,0 +1,28 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `scan-display` command */
+  export type ScanDisplay = ExtensionPreferences & {}
+  /** Preferences accessible in the `scan-multiple` command */
+  export type ScanMultiple = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `scan-display` command */
+  export type ScanDisplay = {}
+  /** Arguments passed to the `scan-multiple` command */
+  export type ScanMultiple = {}
+}
+
